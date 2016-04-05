ad17b37669b4571a0b5fa1edcc6ddb2a42ca99aa
Samuel Gélineau <gelisam+github@gmail.com>
2016-04-04 23:14:42 -0400

    Html results instead of console results

--- HttpGet.elm
+++ HttpGet.elm
@@ -1,14 +1,31 @@
 import Debug
-import Html
+import Effects exposing (Effects, Never)
+import Html exposing (Html)
 import Http
 import Json.Decode as Json exposing ((:=))
-import Task exposing (..)
+import Signal exposing (Signal, Address)
+import StartApp exposing (App)
+import Task exposing (Task)
 
+
+app : App Model
+app =
+  StartApp.start { init = init, view = view, update = update, inputs = [] }
+
+main : Signal Html
+main =
+  app.html
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
+
+
+-- HTTP
 
 lookupZipCode : String -> Task Http.Error (List String)
 lookupZipCode query =
-    Http.get places ("http://localhost:8001/us/" ++ query)
-
+  Http.get places ("http://localhost:8001/us/" ++ query)
 
 places : Json.Decoder (List String)
 places =
@@ -16,12 +33,35 @@ places =
         Json.object2 (\city state -> city ++ ", " ++ state)
           ("place name" := Json.string)
           ("state" := Json.string)
-  in
-      "places" := Json.list place
+  in  "places" := Json.list place
 
-port myPort : Task Http.Error (List String)
-port myPort = lookupZipCode "90210"
-           |> map (\x -> Debug.log (toString x) x)
-           |> mapError (toString >> Debug.crash)
 
-main = Html.text "look at the console"
+-- MODEL
+
+type alias Model = List String
+
+init : (Model, Effects Action)
+init =
+  let task : Task Never Action
+      task = lookupZipCode "90210"
+          |> Task.toResult
+          |> Task.map (toString >> Log)
+  in  ([], Effects.task task)
+
+
+-- UPDATE
+
+type Action
+  = Log String
+
+update : Action -> Model -> (Model, Effects action)
+update (Log string) strings =
+  (strings ++ [string], Effects.none)
+
+
+-- VIEW
+
+view : Address Action -> Model -> Html
+view _ strings =
+  let line string = Html.div [] [Html.text string]
+  in  Html.div [] (List.map line strings)
